d77fa03a6c8c17187c5819caa1bb97de7ec1d326
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-11-22 16:22:22 +0100

    Add info-1 data

--- src/data.ts
+++ src/data.ts
@@ -20,5 +20,74 @@ export const data: PageType = {
         src: "/images/background.png",
       },
     },
+    {
+      _type: "info-1",
+      title: "What is Lorem Ipsum?",
+      body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: {
+        alt: "Info 1 Image",
+        width: 1080,
+        height: 1080,
+        title: "Info 1 Image",
+        src: "/images/yoga.png",
+      },
+      list: [
+        {
+          title: "Avoiding dangerous diseases",
+          body: "Many people consider that diet is a diet by reducing the portion and type of food. But going on a diet is more than both points",
+          icon: {
+            alt: "I",
+            width: 1080,
+            height: 1080,
+            title: "Info 1 List Icon",
+            src: "/images/yoga.png",
+          },
+        },
+        {
+          title: "Avoiding dangerous diseases",
+          body: "Many people consider that diet is a diet by reducing the portion and type of food. But going on a diet is more than both points",
+          icon: {
+            alt: "I",
+            width: 1080,
+            height: 1080,
+            title: "Info 1 List Icon",
+            src: "/images/yoga.png",
+          },
+        },
+        {
+          title: "Avoiding dangerous diseases",
+          body: "Many people consider that diet is a diet by reducing the portion and type of food. But going on a diet is more than both points",
+          icon: {
+            alt: "I",
+            width: 1080,
+            height: 1080,
+            title: "Info 1 List Icon",
+            src: "/images/yoga.png",
+          },
+        },
+        {
+          title: "Avoiding dangerous diseases",
+          body: "Many people consider that diet is a diet by reducing the portion and type of food. But going on a diet is more than both points",
+          icon: {
+            alt: "I",
+            width: 1080,
+            height: 1080,
+            title: "Info 1 List Icon",
+            src: "/images/yoga.png",
+          },
+        },
+        {
+          title: "Avoiding dangerous diseases",
+          body: "Many people consider that diet is a diet by reducing the portion and type of food. But going on a diet is more than both points",
+          icon: {
+            alt: "I",
+            width: 1080,
+            height: 1080,
+            title: "Info 1 List Icon",
+            src: "/images/yoga.png",
+          },
+        },
+      ],
+    },
   ],
 };
